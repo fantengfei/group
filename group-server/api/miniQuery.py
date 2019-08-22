9ab7337edf53d81be8de0b8f41d91fa5a76c3eb2
fantengfei <fantengfeivip@gmail.com>
2019-08-22 20:37:06 +0800

    add file

--- group-server/api/miniQuery.py
+++ group-server/api/miniQuery.py
@@ -4,6 +4,7 @@ from leancloud import Query
 from leancloud import LeanCloudError
 
 from flask import jsonify
+from flask import request
 
 import requests
 import json
@@ -15,29 +16,31 @@ from models import UserInfo, UserSession
 def login(req):
     try:
         code = req.headers['code']
-        _url = 'https://api.weixin.qq.com/sns/jscode2session?appid=wx4ce6fa524f8594c3&' \
-               'secret=a12ea3c16ce994903920c36ddfab646d&js_code=%s&grant_type=authorization_code' % code
+        _url = 'https://api.weixin.qq.com/sns/jscode2session?appid=wx22e8aa5cfa7cab7a&' \
+               'secret=645371539ddd5d6105d7ef5a34541c2c&js_code=%s&grant_type=authorization_code' % code
         rep = requests.get(_url)
         data = rep.json()
+        if 'errmsg' in data:
+            raise BadRequest(data.get('errmsg'))
     except Exception as e:
         raise e
 
     try:
-        session = Query(UserSession).equal_to('openID', data['openid']).first()
+        session = Query(UserSession).equal_to('openID', data.get('openid')).first()
     except LeanCloudError as e:
         if e.code == 101:
             session = UserSession()
         else:
             raise e
 
-    session.set('sessionKey', data['session_key'])
-    session.set('openID', data['openid'])
+    session.set('sessionKey', data.get('session_key'))
+    session.set('openID', data.get('openid'))
     try:
         session.save()
     except LeanCloudError as e:
         raise e
 
-    return jsonify({'openid': data['openid']})
+    return jsonify({'openid': data.get('openid')})
 
 
 def update_user_info(req):
